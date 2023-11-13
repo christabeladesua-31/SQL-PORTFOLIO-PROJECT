839862f313fe1246cb0d6c27ee96298ea7889526
christabeladesua-31 <150682861+christabeladesua-31@users.noreply.github.com>
2023-11-13 14:31:46 +0100

    Add files via upload

--- covid sql query.sql
+++ covid sql query.sql
@@ -7,10 +7,12 @@ order by 3,4
 --from [SQL PORTFOLIO PROJECT]..[covid vaccination]
 --order by 3,4
 
+--SELECT DATA TO BE USED
 select location,date, total_cases, new_cases, total_deaths, population
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
 order by 1,2
 
+--TOTAL CASES VS TOTAL DEATHS 
 select location, date, total_cases,total_deaths, (CONVERT(float,total_deaths)/
 nullif(convert(float, total_cases), 0))*100 as DeathPercentage
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
@@ -18,48 +20,41 @@ where location like '%afghanistan%'
 and continent is not null
 order by 1,2
 
+--TOTAL CASES VS POPULATION
 select location, date, total_cases,population, (CONVERT(float, population)/
 nullif(convert(float, total_cases), 0))*100 as DeathPercentage
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
 order by 1,2
 
+--COUNTRIES WITH THE HIGHEST INFECTION RATE PER POPULATION
 select location, max(total_cases) as HighestInfectionCount,population,
 MAX((total_cases/population)) *100 as PercentPopulationInfected
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
 group by location, population
 order by PercentPopulationInfected desc
 
-
+--COUNTRIES WITH HIGHEST DEATH COUNT PER POPULATION
 select location, max(cast(total_cases as int)) as totaldeathcount
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
 where continent is not null
 group by location
 order by totaldeathcount desc
 
-select location, max(cast(total_cases as int)) as totaldeathcount
-from [SQL PORTFOLIO PROJECT]..[covid deaths ]
-where continent is not null
-group by location
-order by totaldeathcount desc
-
+--CONTINENTS WITH HIGHEST DEATH COUNT PER POPULATION
 select continent, max(cast(total_cases as int)) as totaldeathcount
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
 where continent is not null
 group by continent
 order by totaldeathcount desc
 
-select continent, max(cast(total_cases as int)) as totaldeathcount
-from [SQL PORTFOLIO PROJECT]..[covid deaths ]
-where continent is not null
-group by continent
-order by totaldeathcount desc
-
-select date, sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths,
+--GLOBAL NUMBERS
+select sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths,
 sum(cast(new_deaths as int))/nullif(sum(new_cases),0)*100 as deathpercentage
 from [SQL PORTFOLIO PROJECT]..[covid deaths ]
 where continent is not null
-group by date
+--group by date
 order by 1,2
+
 
 select *
 from [SQL PORTFOLIO PROJECT]..[covid vaccination ] vac
@@ -75,6 +70,7 @@ join [SQL PORTFOLIO PROJECT]..[covid deaths ] dea
 where dea.continent is not null
 order by 2,3
 
+--TOTAL POPULATION VS VACCINATIONS
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , sum(convert(bigint, vac.new_vaccinations)) over (partition by dea.location order by 
   dea.location, dea.date) as peoplevaccinated
@@ -85,6 +81,7 @@ join [SQL PORTFOLIO PROJECT]..[covid deaths ] dea
 where dea.continent is not null
 order by 2,3
 
+--CTE
 with popvsvac (continent, location, date, population, new_vaccinations, peoplevaccinated)
 as
 (
@@ -100,7 +97,7 @@ where dea.continent is not null
 select *, (peoplevaccinated/population)*100
 from popvsvac
 
-
+--TEMP TABLE
 drop table if exists #percentpeoplevaccinated
 create table #percentpeoplevaccinated
 (
@@ -111,7 +108,6 @@ population numeric,
 new_vaccinations numeric,
 peoplevaccinated numeric
 )
-
 
 insert into #percentpeoplevaccinated
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
@@ -126,6 +122,7 @@ join [SQL PORTFOLIO PROJECT]..[covid deaths ] dea
 select *, (peoplevaccinated/population)*100
 from #percentpeoplevaccinated
 
+--VIEW FOR LATER VISUALISATION
 create view percentpeoplevaccinated as
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , sum(convert(bigint, vac.new_vaccinations)) over (partition by dea.location order by 
